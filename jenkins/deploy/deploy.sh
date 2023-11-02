72f3967ae68bcfa3312873c6a40577941dbcd8b1
Ehg-00 <ehg833599@gmail.com>
2023-11-02 16:44:17 +0000

    /opt/prod is modified

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /prod /tmp/.auth ubuntu@13.127.148.3:/tmp/.auth
-scp -i /prod ./jenkins/deploy/publish ubuntu@13.127.148.3:/tmp/publish
-ssh -i /prod ubuntu@13.127.148.3 "/tmp/publish"
+scp -i /opt/prod /tmp/.auth ubuntu@13.127.148.3:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish ubuntu@13.127.148.3:/tmp/publish
+ssh -i /opt/prod ubuntu@13.127.148.3 "/tmp/publish"
