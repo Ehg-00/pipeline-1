442a38436e458fe31a6963dc584de7907bbad49e
Ehg-00 <ehg833599@gmail.com>
2023-11-02 15:55:43 +0000

    files added

---
+++ jenkins/deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo maven > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
+
+scp -i prod /tmp/.auth ubuntu@13.232.128.241:/tmp/.auth
+scp -i prod ./jenkins/deploy/publish ubuntu@13.232.128.241:/tmp/publish
+ssh -i prod ubuntu@13.232.128.241 "/tmp/publish"
